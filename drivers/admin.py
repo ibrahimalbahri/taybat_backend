cd9c6a90bea825529c318d24204ff389dac36256
ibrahim <ibrahim.albahri.ib@gmail.com>
2026-01-14 23:01:48 +0100

    add driver location to admin + adjust session life time

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+
+from drivers.models import DriverLocation
+@admin.register(DriverLocation)
+class DriverLocationAdmin(admin.ModelAdmin):
+    list_display = ("driver", "lat", "lng", "heading", "speed")
+    search_fields = ("driver__id",)
--- taybat_backend/settings.py
+++ taybat_backend/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=500), # ToDO: adjust for production
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
